251676698a8ed77c72940e43a110033a896d2125
Aryan <aryanatwork45@gmail.com>
2026-03-03 22:58:52 +0530

    Update System

--- networkscanning.py
+++ networkscanning.py
@@ -1,33 +1,30 @@
 import socket
-import sys
+from concurrent.futures import ThreadPoolExecutor
 import time
 
-target = input("Enter target IP or hostname:")
+target = input("Enter the target IP address: ") 
+target_ip = socket.gethostbyname(target)
+start_port = int(input("Enter the starting port number: "))
+end_port = int(input("Enter the ending port number: "))
 
-try:
-    target_ip = socket.gethostbyname(target)
-except socket.gaierror:
-    print("Invalid hostname.")
-    sys.exit()
-
-print(f"\nScanning {target_ip} for open ports...")
+print(f"\n Scanning {target_ip}")
 print("-" * 40)
-
-start_port = int(input("Enter start port:"))
-end_port = int(input("Enter end port:"))
 
 start_time = time.time()
 
-for port in range(start_port, end_port + 1):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(1)
-
-    result = s.connect_ex((target_ip, port))
-
-    if result == 0:
-        print(f"Port {port} is OPEN")
-    s.close()
+def scan_port(port):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        result = s.connect_ex((target_ip, port))
+        if result == 0:
+            print(f"[+] Port {port} is open")
+        s.close()
+    except:
+        pass
+with ThreadPoolExecutor(max_workers=100) as executor:
+    for port in range(start_port, end_port + 1):
+        executor.submit(scan_port, port)
 end_time = time.time()
-
 print("-" * 40)
-print(f"Scanning completed in {end_time - start_time:.2f} seconds.")
+print(f"\nScanning completed in {end_time - start_time:.2f} seconds")
